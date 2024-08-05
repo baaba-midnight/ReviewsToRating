2e29bfe0dfd119cb9c8cb8b69f0d0427b3ca500d
Baaba Onomaa Amosah <146720493+baaba-midnight@users.noreply.github.com>
2024-08-05 01:06:08 +0000

    Update settings.py

--- Ratings/settings.py
+++ Ratings/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t7d1mhhhe0yzsm3ai&=@^u1&(7=1^zlwu0$yw_s)*^2zdyeq(@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['review_rank.up.railway.app']
 
 
 # Application definition
